4654759c8886c4f0f23ec4f072e2cf412ffd8a92
Pranjal Chaplot <pranjalchaplot@gmail.com>
2023-07-03 12:43:14 +0530

    initializing routing

--- mernapp/src/App.js
+++ mernapp/src/App.js
@@ -1,13 +1,26 @@
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom';
 import Home from './screens/Home.';
+import Login from './screens/Login';
+import "../node_modules/bootstrap-dark-5/dist/css/bootstrap-dark.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle.js";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+
 
 function App() {
   return (
-    <>
-      <div className='App'>
-        <Home></Home>
+    <Router>
+      <div>
+        <Routes>
+          <Route exact path='/' element={<Home />}></Route>
+          <Route exact path='/login' element={<Login />}></Route>
+        </Routes>
       </div>
-    </>
+    </Router>
   );
 }
 
