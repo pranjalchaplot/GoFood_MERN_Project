5a937a2c1ffb6b2612f3268001373d02ba62d8d0
Pranjal Chaplot <pranjalchaplot@gmail.com>
2023-07-03 12:44:45 +0530

    setting up home screen

--- mernapp/src/screens/Home..js
+++ mernapp/src/screens/Home..js
@@ -1,10 +1,22 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+import ItemCard from '../components/ItemCard'
+import Carousel from '../components/Carousel'
 
 export default function Home() {
     return (
         <div>
-            <Navbar />
+            <div>
+                <Navbar />
+            </div>
+            <div>
+                <Carousel />
+            </div>
+            <div>
+                <ItemCard />
+            </div>
+            <div><Footer /></div>
         </div>
     )
 }
