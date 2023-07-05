bec08d564593001dae89bd4c2b3f1c308284be55
Pranjal Chaplot <pranjalchaplot@gmail.com>
2023-07-05 13:33:05 +0530

    adding CORS

--- mernapp/backend/index.js
+++ mernapp/backend/index.js
@@ -4,13 +4,20 @@ const port = 5000
 const mongoDB = require("./db");
 mongoDB();
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    next();
+});
+
 
 app.use(express.json());
-
 app.use("/api", require('./Routes/CreateUser'));
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+});
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
