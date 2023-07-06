c35367d7232c43609e35713d78ec6a5da50972c9
Pranjal Chaplot <pranjalchaplot@gmail.com>
2023-07-06 18:46:22 +0530

    populating data and adding search feature

--- mernapp/src/components/ItemCard.js
+++ mernapp/src/components/ItemCard.js
@@ -1,13 +1,14 @@
 import React from 'react'
 
-export default function ItemCard() {
+export default function ItemCard(props) {
+    let option = props.options ?? {};
+    let priceOptions = Object.keys(option);
     return (
         <div>
             <div className="card mt-3" style={{ "width": "18rem", "maxHeight": "360px" }}>
-                <img className="card-img-top" src="https://images.unsplash.com/photo-1639330842151-8a92eb332b2d?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=200&ixid=MnwxfDB8MXxyYW5kb218MHx8cHVkZGluZ3x8fHx8fDE2ODgzNjkwNDI&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=300" alt="Item Card" />
+                <img className="card-img-top" src={props.imgURL} alt="Item Card" style={{ height: "120px", objectFit: "fill" }} />
                 <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">Aao Khaana Khaao yaaro!</p>
+                    <h5 className="card-title">{props.cardTitle}</h5>
                     <div className="container w-100">
                         <select name="" id="" className="m-2 h-100 bg-success rounded">
                             {Array.from(Array(6), (e, i) => {
@@ -17,8 +18,13 @@ export default function ItemCard() {
                             })}
                         </select>
                         <select name="" id="" className="m-2 h-100 bg-success rounded">
-                            <option key={1} value="half">Half</option>
-                            <option key={2} value="full">Full</option>
+                            {
+                                priceOptions.map((optionData) => {
+                                    return (
+                                        <option key={optionData} value={optionData}>{optionData}</option>
+                                    )
+                                })
+                            }
                         </select>
                     </div>
                 </div>
--- mernapp/src/screens/Home..js
+++ mernapp/src/screens/Home..js
@@ -1,22 +1,95 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import ItemCard from '../components/ItemCard'
-import Carousel from '../components/Carousel'
 
 export default function Home() {
+    const [Search, setSearch] = useState('')
+    const [FoodItem, setFoodItem] = useState([]);
+    const [FoodCategory, setFoodCategory] = useState([]);
+
+    const loadData = async () => {
+        let response = await fetch("http://localhost:5000/api/foodData", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        });
+
+        let responseJSON = await response.json();
+
+        setFoodItem(responseJSON[0]);
+        setFoodCategory(responseJSON[1]);
+    };
+
+    const searchHandler = async (e) => {
+        setSearch(e.target.value);
+    }
+
+    useEffect(() => {
+        loadData();
+    }, []);
     return (
         <div>
             <div>
                 <Navbar />
             </div>
             <div>
-                <Carousel />
+                <div id="carouselExampleFade" className="carousel slide carousel-fade" data-bs-ride="carousel" style={{ objectFit: "contain !important" }}>
+                    <div className="carousel-inner" id='carousel'>
+                        <div className="carousel-caption" style={{ zIndex: "10" }}>
+                            <div className="d-flex justify-content-center">
+                                <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" value={Search} onChange={searchHandler} />
+                            </div>
+                        </div>
+                        <div className="carousel-item active">
+                            <img src="https://source.unsplash.com/random/900x700/?burger" className="d-block w-100" style={{ filter: "brightness(30%)" }} alt="..." />
+                        </div>
+                        <div className="carousel-item">
+                            <img src="https://source.unsplash.com/random/900x700/?paneer-pakode" className="d-block w-100" style={{ filter: "brightness(30%)" }} alt="..." />
+                        </div>
+                        <div className="carousel-item">
+                            <img src="https://source.unsplash.com/random/900x700/?brownie" className="d-block w-100" style={{ filter: "brightness(30%)" }} alt="..." />
+                        </div>
+                    </div>
+                    <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+                        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span className="visually-hidden">Previous</span>
+                    </button>
+                    <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+                        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span className="visually-hidden">Next</span>
+                    </button>
+                </div>
             </div>
-            <div className="m-3">
-                <ItemCard />
-                <ItemCard />
-                <ItemCard />
+            <div className="container">
+                {
+                    FoodCategory !== [] ? FoodCategory.map((category) => {
+                        return (
+                            <div className='row m-3'>
+                                <div key={category._id} className='fs-3 m-3'>{category.CategoryName}</div>
+                                <hr />
+                                {
+                                    FoodItem !== [] ? FoodItem.filter((foodItem) =>
+                                        (foodItem.CategoryName === category.CategoryName)
+                                        && (foodItem.name.toLowerCase().includes(Search.toLowerCase()))
+                                    ).map((item) => {
+                                        return (
+                                            <>
+                                                <div key={item._id} className='col-12 col-md-6 col-lg-3'>
+                                                    <ItemCard
+                                                        cardTitle={item.name}
+                                                        imgURL={item.img}
+                                                        options={item.options[0]} />
+                                                </div>
+                                            </>
+                                        )
+                                    }) : <div></div>
+                                }
+                            </div>
+                        );
+                    }) : <div></div>
+                }
             </div>
             <div><Footer /></div>
         </div>
