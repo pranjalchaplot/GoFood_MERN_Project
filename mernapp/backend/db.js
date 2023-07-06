4e8d9d6ca6bf1a2953519e7563ca2a222d8ff472
Pranjal Chaplot <pranjalchaplot@gmail.com>
2023-07-06 18:45:20 +0530

    sending foodItem and foodCategory data to ui

--- mernapp/backend/db.js
+++ mernapp/backend/db.js
@@ -5,9 +5,14 @@ const mongoDB = async () => {
     try {
         await mongoose.connect(mongoURI);
         console.log('Connected!');
-        let fetched_data = mongoose.connection.db.collection("food_items");
-        let data = await fetched_data.find({}).toArray()
-        console.log();
+        let foodItems = await mongoose.connection.db.collection("food_items");
+        let itemData = await foodItems.find({}).toArray();
+        global.food_items = itemData;
+
+        let foodCategory = await mongoose.connection.db.collection("foodCategory");
+        let categoryData = await foodCategory.find({}).toArray();
+        global.foodCategory = categoryData;
+
     } catch (error) {
         console.log('err: ', error);
     }
